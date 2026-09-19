19a2a7cc4a5cfa5b29380c5319fcec911c2775c5
Ryence07 <ryences.cortez@gmail.com>
2026-09-20 00:30:46 +0800

    Add Footer component with copyright and tagline

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 PaddleMatch</p>
+      <p>Find players. Find paddles. Play better.</p>
+    </footer>
+  )
+}
+
+export default Footer
